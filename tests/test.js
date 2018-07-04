50e08de929731fcfb490272bcdb6033caa56a0c3
JimTheMan <mrdotjim@gmail.com>
2018-07-04 00:22:55 -0400

    ok then

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,8 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-        test.string(result.body.Output).contains('Hello');
+      console.log('result ', result.body.Output)
+        test.string(result.body.Output).contains('coolio');
         test.value(result).hasHeader('content-type', 'application/json; charset=utf-8');
         done(err);
     });
